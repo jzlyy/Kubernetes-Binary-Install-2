0524bd216d22b7cdd1d0a73405fc6335a88bd719
root <wwyxhqc1@foxmail.com>
2025-03-19 11:34:02 +0800

    Install containerd environment

--- scripts/04-containerd-install.sh
+++ scripts/04-containerd-install.sh
@@ -3,8 +3,8 @@
 ######Install containerd environment######
 
 #Download crictl interaction tool
-sudo wget https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.26.1/crictl-v1.32.0-linux-amd64.tar.gz
-sudo tar zxvf crictl-v1.32.0-linux-amd64.tar.gz -C /usr/local/bin
+#sudo wget https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.32.0/crictl-v1.32.0-linux-amd64.tar.gz
+#sudo tar zxvf crictl-v1.32.0-linux-amd64.tar.gz -C /usr/local/bin
 
 #Edit containerd configuration file
 sudo containerd config default > /etc/containerd/config.toml
@@ -12,16 +12,19 @@ sudo sed -i 's#sandbox_image\s*=\s*"registry.k8s.io/pause:3.8"#sandbox_image="re
 sudo sed -i 's#SystemdCgroup\s*=\s*false#SystemdCgroup = true#g' /etc/containerd/config.toml
 sudo sed -i 's#config_path\s*=\s*""#config_path = "/etc/containerd/certs.d"#g' /etc/containerd/config.toml
 
+#Start containerd service
+sudo systemctl enable containerd
+sudo systemctl start containerd
+
 #Configure containerd image accelerator
 sudo mkdir -p /etc/containerd/certs.d/docker.io
-sudo cp /root/kubernetes-binary-install/configs/containerd/containerd.tolm /etc/containerd/certs.d/docker.io/hosts.toml
+sudo cp /root/kubernetes-binary-install-2/configs/containerd/containerd.tolm /etc/containerd/certs.d/docker.io/hosts.toml
 
 #Configure runtime and image pulling to use containerd
-sudo cp /root/kubernetes-binary-install/configs/containerd/crictl.yaml /etc/crictl.yaml
+sudo cp /root/kubernetes-binary-install-2/configs/containerd/crictl.yaml /etc/crictl.yaml
 
-#Start containerd service
+#Restart containerd service
 sudo systemctl daemon-reload
-sudo systemctl enable containerd
 sudo systemctl start containerd
 
 #Set up Container Runtime Interface (CRI) for containerd
