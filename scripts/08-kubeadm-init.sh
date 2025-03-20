d61a9b147aee6bf2d323e3268447582bd9e25dfa
root <wwyxhqc1@foxmail.com>
2025-03-20 15:02:25 +0800

    Initialize the Cluster with kubeadm

--- scripts/08-kubeadm-init.sh
+++ scripts/08-kubeadm-init.sh
@@ -2,14 +2,7 @@
 
 ######Initialize the Cluster with kubeadm######
 
-sudo kubeadm init \
---apiserver-advertise-address=172.168.20.10 \
---control-plane-endpoint=172.168.20.10:16443 --upload-certs \
---kubernetes-version=v1.32.3 \
---image-repository registry.aliyuncs.com/google_containers \
---cri-socket unix:///run/containerd/containerd.sock \
---pod-network-cidr=10.244.0.0/16 \
---service-cidr=10.96.0.0/12
+sudo kubeadm init --config=/root/kubernetes-binary-install-2/configs/kubeadm-config.yaml --upload-certs
 
 #Set Up kubectl Access
 sudo mkdir -p $HOME/.kube
