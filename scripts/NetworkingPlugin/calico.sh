5951b014555b82ef9695ab2c874d214432251705
root <wwyxhqc1@foxmail.com>
2025-03-21 20:52:52 +0800

    Install Calico Networking Plugin

---
+++ scripts/NetworkingPlugin/calico.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+######Install Calico Networking Plugin######
+
+sudo kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.29.2/manifests/tigera-operator.yaml
+sudo kubectl apply -f /root/kubernetes-binary-install/manifests/calico/resources.yaml
