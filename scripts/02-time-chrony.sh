f9afaa3be61b44075057dc948ad7329eb3153293
root <wwyxhqc1@foxmail.com>
2025-03-23 09:25:28 +0800

    Delete obsolete scripts

--- scripts/02-time-chrony.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-
-######Configure Chrony for time synchronization######
-
-sudo cat /root/kubernetes-binary-install-2/configs/chrony.conf >> /etc/chrony.conf
-sudo systemctl restart chronyd
