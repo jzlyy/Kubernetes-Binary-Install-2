799653c8ddbe16dd62ba08a13f0f974e8b9631fb
root <wwyxhqc1@foxmail.com>
2025-03-23 20:30:15 +0800

    Network Configuration Rules

--- scripts/cluster/worker/01-network-rules.sh
+++ scripts/cluster/worker/01-network-rules.sh
@@ -3,18 +3,18 @@
 ######Network Configuration Rules######
 
 #Load the br_netfilter Kernel Module
-sudo cp /root/kubernetes-binary-install-2/configs/modules-load.conf /etc/modules-load.d/k8s.conf
+sudo cp /root/Kubernetes-Binary-Install-2/configs/modules-load.conf /etc/modules-load.d/k8s.conf
 sudo systemctl restart systemd-modules-load.service
 
 #Modify Linux Kernel Parameters (enable bridge traffic forwarding)
-sudo cp /root/kubernetes-binary-install-2/configs/network.conf /etc/sysctl.d/k8s.conf
+sudo cp /root/Kubernetes-Binary-Install-2/configs/network.conf /etc/sysctl.d/k8s.conf
 sudo sysctl -p /etc/sysctl.d/k8s.conf
 
 #Define IPVS Prerequisites (for kube-proxy initialization)
 #Download IPVS packages
 sudo dnf -y install ipvsadm
 sudo mkdir -p /etc/sysconfig/modules
-sudo cp /root/kubernetes-binary-install-2/configs/ipvs.conf /etc/sysconfig/modules/ipvs.modules
+sudo cp /root/Kubernetes-Binary-Install-2/configs/ipvs.conf /etc/sysconfig/modules/ipvs.modules
 
 #Apply/Source the Script File
 sudo chmod 755 /etc/sysconfig/modules/ipvs.modules
