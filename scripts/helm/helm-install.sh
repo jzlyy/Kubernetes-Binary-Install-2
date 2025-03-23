d33883c42aafcac15c58cc377a07d7b13c87c923
root <wwyxhqc1@foxmail.com>
2025-03-23 17:19:21 +0800

    Install Helm

--- scripts/helm/helm-install.sh
+++ scripts/helm/helm-install.sh
@@ -2,8 +2,7 @@
 
 ######Install Helm######
 
+# Download helm tool
 wget https://get.helm.sh/helm-v3.17.2-linux-amd64.tar.gz
-
 tar -xf helm-v3.17.2-linux-amd64.tar.gz
-
 mv linux-amd64/helm /usr/local/bin 
