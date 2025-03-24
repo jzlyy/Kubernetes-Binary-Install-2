840246d5d6312d138bc4dd7f39d23fdab4f87cdc
root <wwyxhqc1@foxmail.com>
2025-03-24 13:35:39 +0800

    Global Operation

--- scripts/test_cluster-worker.sh
+++ scripts/test_cluster-worker.sh
@@ -2,7 +2,6 @@
 
 ######Global operation######
 
-sh /root/kubernetes-binary-install-2/scripts/cluster/worker/00-preflight.sh
 sh /root/kubernetes-binary-install-2/scripts/cluster/worker/01-network-rules.sh
 sh /root/kubernetes-binary-install-2/scripts/cluster/worker/02-time-chrony.sh
 sh /root/kubernetes-binary-install-2/scripts/cluster/worker/03-docker-install.sh
