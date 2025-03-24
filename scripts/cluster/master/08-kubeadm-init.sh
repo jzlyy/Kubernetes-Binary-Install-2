ab96624ef8bac447fff8d1cd9aba9b201cf34bb4
root <wwyxhqc1@foxmail.com>
2025-03-24 18:17:17 +0800

    Initialize the Cluster with kubeadm

--- scripts/cluster/master/08-kubeadm-init.sh
+++ scripts/cluster/master/08-kubeadm-init.sh
@@ -5,6 +5,6 @@
 sudo kubeadm init --config=/root/kubernetes-binary-install-2/configs/kubeadm-config.yaml --upload-certs
 
 #Set Up kubectl Access
-sudo mkdir -p ~/.kube
-sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
+sudo mkdir -p "$HOME/.kube"
+sudo cp -i /etc/kubernetes/admin.conf "$HOME/.kube/config"
 sudo -E chown "$(id -u):$(id -g) $HOME/.kube/config"
