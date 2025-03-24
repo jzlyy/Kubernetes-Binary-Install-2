38b3afa370d5337e61a48539a01dd7f6e829fa3a
root <wwyxhqc1@foxmail.com>
2025-03-24 17:36:21 +0800

    Install containerd environment

--- scripts/cluster/worker/04-containerd-install.sh
+++ scripts/cluster/worker/04-containerd-install.sh
@@ -7,7 +7,7 @@ sudo wget https://github.com/kubernetes-sigs/cri-tools/releases/download/v1.32.0
 sudo tar zxvf crictl-v1.32.0-linux-amd64.tar.gz -C /usr/local/bin
 
 #Edit containerd configuration file
-sudo containerd config default > /etc/containerd/config.toml
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null
 sudo sed -i 's#sandbox_image\s*=\s*"registry.k8s.io/pause:3.8"#sandbox_image="registry.aliyuncs.com/google_containers/pause:3.10"#g' /etc/containerd/config.toml
 sudo sed -i 's#SystemdCgroup\s*=\s*false#SystemdCgroup = true#g' /etc/containerd/config.toml
 sudo sed -i 's#config_path\s*=\s*""#config_path = "/etc/containerd/certs.d"#g' /etc/containerd/config.toml
