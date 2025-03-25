fcfaaa71984131fa4f82547fc21641efeb4d93c2
root <wwyxhqc1@foxmail.com>
2025-03-25 13:53:28 +0800

    Global Operation

--- scripts/test_cluster-master.sh
+++ scripts/test_cluster-master.sh
@@ -2,13 +2,13 @@
 
 ######Global operation######
 
-sh /root/kubernetes-binary-install-2/scripts/cluster/master/01-network-rules.sh
-sh /root/kubernetes-binary-install-2/scripts/cluster/master/02-time-chrony.sh
-sh /root/kubernetes-binary-install-2/scripts/cluster/master/03-docker-install.sh
-sh /root/kubernetes-binary-install-2/scripts/cluster/master/04-containerd-install.sh
-sh /root/kubernetes-binary-install-2/scripts/cluster/master/05-keepalived+nginx.sh
-sh /root/kubernetes-binary-install-2/scripts/cluster/master/06-kubernetes-yum.sh
-sh /root/kubernetes-binary-install-2/scripts/cluster/master/07-kubenetes-tool-install.sh
-sh /root/kubernetes-binary-install-2/scripts/cluster/master/08-kubeadm-init.sh
-sh /root/kubernetes-binary-install-2/scripts/cluster/master/networking-plugin/calico.sh
+sh scripts/cluster/master/01-network-rules.sh
+sh scripts/cluster/master/02-time-chrony.sh
+sh scripts/cluster/master/03-docker-install.sh
+sh scripts/cluster/master/04-containerd-install.sh
+sh scripts/cluster/master/05-keepalived+nginx.sh
+sh scripts/cluster/master/06-kubernetes-yum.sh
+sh scripts/cluster/master/07-kubenetes-tool-install.sh
+sh scripts/cluster/master/08-kubeadm-init.sh
+sh scripts/cluster/master/networking-plugin/calico.sh
 
