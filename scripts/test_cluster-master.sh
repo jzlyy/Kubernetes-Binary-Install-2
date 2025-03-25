a6158ba3e43930cbda7c8e2beec994018ec1af3f
root <wwyxhqc1@foxmail.com>
2025-03-25 15:55:53 +0800

    Global Operation

--- scripts/test_cluster-master.sh
+++ scripts/test_cluster-master.sh
@@ -2,13 +2,13 @@
 
 ######Global operation######
 
-sh scripts/cluster/master/01-network-rules.sh
-sh scripts/cluster/master/02-time-chrony.sh
-sh scripts/cluster/master/03-docker-install.sh
-sh scripts/cluster/master/04-containerd-install.sh
-sh scripts/cluster/master/05-keepalived+nginx.sh
-sh scripts/cluster/master/06-kubernetes-yum.sh
-sh scripts/cluster/master/07-kubenetes-tool-install.sh
-sh scripts/cluster/master/08-kubeadm-init.sh
-sh scripts/cluster/master/networking-plugin/calico.sh
+sh cluster/master/01-network-rules.sh
+sh cluster/master/02-time-chrony.sh
+sh cluster/master/03-docker-install.sh
+sh cluster/master/04-containerd-install.sh
+sh cluster/master/05-keepalived+nginx.sh
+sh cluster/master/06-kubernetes-yum.sh
+sh cluster/master/07-kubenetes-tool-install.sh
+sh cluster/master/08-kubeadm-init.sh
+sh cluster/master/networking-plugin/calico.sh
 
