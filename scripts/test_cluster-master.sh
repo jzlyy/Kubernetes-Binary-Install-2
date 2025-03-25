3b98c0c7c863fcbed0854a7cad40a6fb10394ac1
root <wwyxhqc1@foxmail.com>
2025-03-25 16:33:19 +0800

    Global Operation

--- scripts/test_cluster-master.sh
+++ scripts/test_cluster-master.sh
@@ -2,16 +2,12 @@
 
 ######Global operation######
 
-set -euxo pipefail
-
-# 动态获取脚本路径
-SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
-CLUSTER_DIR="${SCRIPT_DIR}/cluster/master"
-
-# 按顺序执行子脚本
-"${CLUSTER_DIR}/00-network-rules.sh"
-"${CLUSTER_DIR}/01-time-chrony.sh"
-"${CLUSTER_DIR}/02-docker-install.sh"
-
-# 执行网络插件脚本
-"${CLUSTER_DIR}/networking-plugin/calico.sh"
+sh cluster/master/01-network-rules.sh
+sh cluster/master/02-time-chrony.sh
+sh cluster/master/03-docker-install.sh
+sh cluster/master/04-containerd-install.sh
+sh cluster/master/05-keepalived+nginx.sh
+sh cluster/master/06-kubernetes-yum.sh
+sh cluster/master/07-kubenetes-tool-install.sh
+sh cluster/master/08-kubeadm-init.sh
+sh cluster/master/networking-plugin/calico.sh
