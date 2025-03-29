cc1bb9176c9ee61ee282756ba5d8b7bc5bee5c3c
root <wwyxhqc1@foxmail.com>
2025-03-29 10:13:44 +0800

    Install Etcd

---
+++ scripts/cluster/etcd/etcd.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+######安装etcd######
+
+#下载etcd工具包
+sudo wget https://github.com/etcd-io/etcd/releases/download/v3.5.21/etcd-v3.5.21-linux-amd64.tar.gz
+
+sudo tar -xf etcd-v3.5.21-linux-amd64.tar.gz
+sudo mv etcd-v3.5.21-linux-amd64/etcd* /usr/local/bin
+
+#创建目录和权限配置
+sudo mkdir -p /etc/etcd /var/lib/etcd
+sudo chmod 700 /var/lib/etcd
+sudo mv ca.pem etcd.pem etcd-key.pem /etc/etcd/
+
+#配置etcd服务文件
+sudo cp /root/kubernetes-binary-install-2/configs/etcd/etcd-01.service /etc/systemd/system/etcd-01.service
+
+#启动etcd
+sudo systemctl daemon-reload
+sudo systemctl enable etcd
+sudo systemctl start etcd
+
+
