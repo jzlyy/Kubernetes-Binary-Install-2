ee8980394356cd3c14debe9418733796e74a5fc8
root <wwyxhqc1@foxmail.com>
2025-03-29 17:54:47 +0800

    Configure Chrony for time synchronization

--- scripts/cluster/master-backup/02-time-chrony.sh
+++ scripts/cluster/master-backup/02-time-chrony.sh
@@ -2,5 +2,5 @@
 
 ######Configure Chrony for time synchronization######
 
-sudo cat /root/kubernetes-binary-install-2/configs/chrony.conf | sudo tee -a /etc/chrony.conf >/dev/null
+sudo cat /root/kubernetes-binary-install/configs/chrony.conf | sudo tee -a /etc/chrony.conf >/dev/null
 sudo systemctl restart chronyd
