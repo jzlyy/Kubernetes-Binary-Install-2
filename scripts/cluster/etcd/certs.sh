f0e4214a5f33a25d369834071055946f31be65c7
root <wwyxhqc1@foxmail.com>
2025-03-29 10:13:24 +0800

    cert

---
+++ scripts/cluster/etcd/certs.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+######创建证书######
+
+#下载证书工具
+sudo wget https://github.com/cloudflare/cfssl/releases/download/v1.6.4/cfssl_1.6.4_linux_amd64
+sudo wget https://github.com/cloudflare/cfssl/releases/download/v1.6.4/cfssljson_1.6.4_linux_amd64
+sudo wget https://github.com/cloudflare/cfssl/releases/download/v1.6.4/cfssl-certinfo_1.6.4_linux_amd64
+
+#生成可执行变量
+sudo chmod +x cfssl_1.6.4_linux_amd64 cfssl-certinfo_1.6.4_linux_amd64 cfssljson_1.6.4_linux_amd64
+sudo mv cfssl_1.6.4_linux_amd64 /usr/local/bin/cfssl
+sudo mv cfssljson_1.6.4_linux_amd64 /usr/local/bin/cfssljson
+sudo mv cfssl-certinfo_1.6.4_linux_amd64 /usr/local/bin/cfssl-certinfo
+
+#创建CA证书
+sudo cp /root/kubernetes-binary-install-2/configs/etcd/ca-config.json ca-config.json
+sudo cp /root/kubernetes-binary-install-2/configs/etcd/ca-csr.json ca-csr.json
+sudo cp /root/kubernetes-binary-install-2/configs/etcd/etcd-csr.json etcd-csr.json
+
+#生成证书
+sudo cfssl gencert -initca ca-csr.json | cfssljson -bare ca
+
+sudo cfssl gencert \
+  -ca=ca.pem \
+  -ca-key=ca-key.pem \
+  -config=ca-config.json \
+  -profile=etcd etcd-csr.json | cfssljson -bare etcd
