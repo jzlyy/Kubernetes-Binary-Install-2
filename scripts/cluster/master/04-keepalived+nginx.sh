72fccfbcd066a021f0f40c79aff8c400e7f72b63
root <wwyxhqc1@foxmail.com>
2025-03-29 17:47:45 +0800

    Set up Layer 4 load balancing with keepalived and Nginx

--- scripts/cluster/master/04-keepalived+nginx.sh
+++ scripts/cluster/master/04-keepalived+nginx.sh
@@ -8,13 +8,13 @@ sudo dnf -y install nginx keepalived
 sudo dnf -y install nginx-all-modules.noarch
 
 #Modify Nginx configuration file
-sudo cat /root/kubernetes-binary-install-2/configs/loadbalancer/nginx.conf | sudo tee -a /etc/nginx/nginx.conf >/dev/null
+sudo cat /root/kubernetes-binary-install/configs/loadbalancer/nginx.conf | sudo tee -a /etc/nginx/nginx.conf >/dev/null
 
 #Modify keepalived configuration file (Master)
-sudo cp /root/kubernetes-binary-install-2/configs/loadbalancer/master-keepalived.conf /etc/keepalived/keepalived.conf
+sudo cp /root/kubernetes-binary-install/configs/loadbalancer/master-keepalived.conf /etc/keepalived/keepalived.conf
 
 #Configure Nginx health checks
-sudo cp /root/kubernetes-binary-install-2/configs/loadbalancer/check_nginx.sh /etc/keepalived/check_nginx.sh
+sudo cp /root/kubernetes-binary-install/configs/loadbalancer/check_nginx.sh /etc/keepalived/check_nginx.sh
 chmod +x /etc/keepalived/check_nginx.sh
 
 #Start Nginx and keepalived services
