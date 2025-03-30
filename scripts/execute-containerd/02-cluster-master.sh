3e06608be989aae86148eba10861911704c40d52
root <wwyxhqc1@foxmail.com>
2025-03-30 14:08:39 +0800

    Execute Cluster Master

--- scripts/execute-containerd/02-cluster-master.sh
+++ scripts/execute-containerd/02-cluster-master.sh
@@ -9,5 +9,5 @@ sh /root/kubernetes-binary-install/scripts/cluster/master/03-containerd/02-conta
 sh /root/kubernetes-binary-install/scripts/cluster/master/04-keepalived+nginx.sh
 sh /root/kubernetes-binary-install/scripts/cluster/master/05-kubernetes-yum.sh
 sh /root/kubernetes-binary-install/scripts/cluster/master/06-kubenetes-tool-install.sh
-sh /root/kubernetes-binary-install/scripts/cluster/master/07-kubeadm-init.sh
+sh /root/kubernetes-binary-install/scripts/cluster/master/07-kubeadm-init-containerd.sh
 sh /root/kubernetes-binary-install/scripts/cluster/master/networking-plugin/calico.sh
