5158be56c030bfb29eb34733def12351fe612f41
root <wwyxhqc1@foxmail.com>
2025-03-30 11:21:02 +0800

    Create Certificate

--- scripts/cluster/etcd/certificate.sh
+++ scripts/cluster/etcd/certificate.sh
@@ -13,25 +13,32 @@ sudo mv cfssl_1.6.4_linux_amd64 /usr/local/bin/cfssl
 sudo mv cfssljson_1.6.4_linux_amd64 /usr/local/bin/cfssljson
 sudo mv cfssl-certinfo_1.6.4_linux_amd64 /usr/local/bin/cfssl-certinfo
 
-#Create CA certificate
-sudo cp /root/kubernetes-binary-install-2/configs/etcd/ca-config.json ca-config.json
-sudo cp /root/kubernetes-binary-install-2/configs/etcd/ca-csr.json ca-csr.json
-sudo cp /root/kubernetes-binary-install-2/configs/etcd/etcd-csr.json etcd-csr.json
-
-#Generate certificates
+#创建CA文件和请求文件(只在一个主机在执行)
+sudo cp /root/kubernetes-binary-install/configs/etcd/ca-config.json ca-config.json
+sudo cp /root/kubernetes-binary-install/configs/etcd/ca-csr.json ca-csr.json
 cfssl gencert -initca ca-csr.json | cfssljson -bare ca
 
+#创建服务器请求文件
+sudo cp /root/kubernetes-binary-install/configs/etcd/etcd-csr.json etcd-csr.json
 cfssl gencert \
   -ca=ca.pem \
   -ca-key=ca-key.pem \
   -config=ca-config.json \
   -profile=etcd etcd-csr.json | cfssljson -bare etcd
 
+#创建客户端证书请求文件
+sudo cp /root/kubernetes-binary-install/configs/etcd/client-csr.json client-csr.json
+cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=etcd client-csr.json | cfssljson -bare client
+
+#创建对等体请求文件
+sudo cp /root/kubernetes-binary-install/configs/etcd/peer-csr.json peer-csr.json
+cfssl gencert -ca=ca.pem -ca-key=ca-key.pem -config=ca-config.json -profile=etcd peer-csr.json | cfssljson -bare peer
+
 #Create directories and configure permissions
 sudo mkdir -p /etc/etcd /var/lib/etcd
 sudo chmod 700 /var/lib/etcd
-sudo mv ca.pem etcd.pem etcd-key.pem /etc/etcd/
+sudo mv ca.pem etcd.pem etcd-key.pem peer.pem peer-key.pem clinet.pem client-key.pem /etc/etcd/
 
 #Ensure a unified trust chain (CA)
-sudo scp /etc/etcd/{ca.pem,etcd.pem,etcd-key.pem} 172.168.20.121:/etc/etcd/
-sudo scp /etc/etcd/{ca.pem,etcd.pem,etcd-key.pem} 172.168.20.122:/etc/etcd/
+sudo scp /etc/etcd/{ca.pem,etcd.pem,etcd-key.pem,peer.pem,peer-key.pem} 172.168.20.121:/etc/etcd/
+sudo scp /etc/etcd/{ca.pem,etcd.pem,etcd-key.pem,peer.pem,peer-key.pem} 172.168.20.122:/etc/etcd/
