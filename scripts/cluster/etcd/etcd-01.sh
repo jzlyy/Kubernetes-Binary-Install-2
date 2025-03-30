9af084d077fc0f6e0bae7fa918be8e6dd5f4c261
root <wwyxhqc1@foxmail.com>
2025-03-30 11:28:45 +0800

    Install Etcd

--- scripts/cluster/etcd/etcd-01.sh
+++ scripts/cluster/etcd/etcd-01.sh
@@ -9,7 +9,7 @@ sudo tar -xf etcd-v3.5.21-linux-amd64.tar.gz
 sudo mv etcd-v3.5.21-linux-amd64/etcd* /usr/local/bin
 
 #Configure etcd service file
-sudo cp /root/kubernetes-binary-install-2/configs/etcd/etcd-01.service /etc/systemd/system/etcd-01.service
+sudo cp /root/kubernetes-binary-install/configs/etcd/etcd-01.service /etc/systemd/system/etcd-01.service
 
 #Start etcd service
 sudo systemctl daemon-reload
